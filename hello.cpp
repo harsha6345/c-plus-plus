48a6de0eb0098d70a03de703ae0842b1d9b3cf4a
harsha6345 <harshab6345@gmail.com>
2023-10-11 22:44:18 +0530

    something

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 int main()
 {
-    int myNum = 15;
-    cout << myNum;
-    int myNum = 5;            // Integer (whole number without decimals)
-    double myFloatNum = 5.99; // Floating point number (with decimals)
-    char myLetter = 'D';      // Character
-    string myText = "Hello";  // String (text)
-    bool myBoolean = true;    // Boolean (true or false)
+    string name = "Harsha";
+    cout << "My name is " << name;
     return 0;
 }
