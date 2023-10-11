6175c968864c721ea401ca61d91c85c5f26e3112
harsha6345 <harshab6345@gmail.com>
2023-10-11 23:24:37 +0530

    backlashes

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    string something = "Hello world";
-    int length = something.length();
-    cout << length;
+    string txt = "We are the so-called \"Vikings\" from the north.";
+    txt = "It\'s alright.";
+    txt = "The character \\ is called backslash.";
+    cout << txt;
 }
