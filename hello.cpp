d9fbaa5aaf29053e46db9877b4293f242561d6cc
harsha6345 <harshab6345@gmail.com>
2023-10-11 23:19:41 +0530

    length

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 int main()
 {
-    int x, y;
-    int sum;
-    cout << "Type a number: ";
-    cin >> x;
-    cout << "Type another number: ";
-    cin >> y;
-    sum = x + y;
-    cout << "Sum is: " << sum;
+    string something = "Hello world";
+    int length = something.length();
+    cout << length;
 }
