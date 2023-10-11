43cb17bea8c0cc2c5a99ea290640c02564166f57
harsha6345 <harshab6345@gmail.com>
2023-10-11 22:46:56 +0530

    sum c++

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main()
 {
-    string name = "Harsha";
-    cout << "My name is " << name;
-    return 0;
+    int x, y;
+    int sum;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Sum is: " << sum;
 }
